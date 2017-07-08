46a7e8d391b39e566542d065caf24447f2a61e0b
Tom Wolcott <ph03n1x.wiz@gmail.com>
2017-07-07 21:27:50 -0400

    Initial commit for HelpingHandsTest

---
+++ app/assets/javascripts/react/react-libraries.js
@@ -0,0 +1,8 @@
+//React Library
+React = require('react');
+//Material Design Library
+MaterialUi = require('material-ui/lib');
+injectTapEventPlugin = require('react-tap-event-plugin'); injectTapEventPlugin();
+//Material Design Library Custom Theme
+MyRawTheme = require('./theme');
+ThemeManager = require('material-ui/lib/styles/theme-manager');
